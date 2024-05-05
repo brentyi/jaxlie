Broadcasting tests, bug fixes

jaxlie/_se3.py
@@ -139,13 +139,16 @@ class SE3(_base.SEBase[SO3]):
 
         skew_omega = _skew(tangent[..., 3:])
         V = jnp.where(
-            use_taylor,
+            use_taylor[..., None, None],
             rotation.as_matrix(),
             (
                 jnp.eye(3)
-                + (1.0 - jnp.cos(theta_safe)) / (theta_squared_safe) * skew_omega
-                + (theta_safe - jnp.sin(theta_safe))
-                / (theta_squared_safe * theta_safe)
+                + ((1.0 - jnp.cos(theta_safe)) / (theta_squared_safe))[..., None, None]
+                * skew_omega
+                + (
+                    (theta_safe - jnp.sin(theta_safe))
+                    / (theta_squared_safe * theta_safe)
+                )[..., None, None]
                 * jnp.einsum("...ij,...jk->...ik", skew_omega, skew_omega)
             ),
         )

jaxlie/_so2.py
@@ -8,7 +8,7 @@ from jax import numpy as jnp
 from typing_extensions import override
 
 from . import _base, hints
-from .utils import register_lie_group
+from .utils import register_lie_group, broadcast_leading_axes
 
 
 @register_lie_group(
@@ -88,7 +88,8 @@ class SO2(_base.SOBase):
 
     @override
     def apply(self, target: hints.Array) -> jax.Array:
-        assert target.shape == (*self.get_batch_axes(), 2)
+        assert target.shape[-1:] == (2,)
+        self, target = broadcast_leading_axes((self, target))
         return jnp.einsum("...ij,...j->...i", self.as_matrix(), target)
 
     @override

jaxlie/_so3.py
@@ -8,7 +8,7 @@ from jax import numpy as jnp
 from typing_extensions import override
 
 from . import _base, hints
-from .utils import get_epsilon, register_lie_group
+from .utils import broadcast_leading_axes, get_epsilon, register_lie_group
 
 
 @register_lie_group(
@@ -299,7 +299,8 @@ class SO3(_base.SOBase):
 
     @override
     def apply(self, target: hints.Array) -> jax.Array:
-        assert target.shape == (*self.get_batch_axes(), 3)
+        assert target.shape[-1:] == (3,)
+        self, target = broadcast_leading_axes((self, target))
 
         # Compute using quaternion multiplys.
         padded_target = jnp.concatenate(

jaxlie/utils/_utils.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING, Callable, Type, TypeVar
+from typing import TYPE_CHECKING, Callable, Tuple, Type, TypeVar, Union, cast
 
 from jax import numpy as jnp
+from jaxlie.hints import Array
 
 if TYPE_CHECKING:
     from .._base import MatrixLieGroup
@@ -55,3 +56,40 @@ def register_lie_group(
         return cls
 
     return _wrap
+
+
+TupleOfBroadcastable = TypeVar(
+    "TupleOfBroadcastable",
+    bound="Tuple[Union[MatrixLieGroup, Array], ...]",
+)
+
+
+def broadcast_leading_axes(inputs: TupleOfBroadcastable) -> TupleOfBroadcastable:
+    """Broadcast leading axes of arrays. Takes tuples of either:
+    - an array, which we assume has shape (*, D).
+    - a Lie group object."""
+
+    from .._base import MatrixLieGroup
+
+    array_inputs = [
+        (x.parameters(), (x.parameters_dim,))
+        if isinstance(x, MatrixLieGroup)
+        else (x, x.shape[-1:])
+        for x in inputs
+    ]
+    for array, shape_suffix in array_inputs:
+        assert array.shape[-len(shape_suffix) :] == shape_suffix
+    batch_axes = jnp.broadcast_shapes(
+        *[array.shape[: -len(suffix)] for array, suffix in array_inputs]
+    )
+    broadcasted_arrays = tuple(
+        jnp.broadcast_to(array, batch_axes + shape_suffix)
+        for (array, shape_suffix) in array_inputs
+    )
+    return cast(
+        TupleOfBroadcastable,
+        tuple(
+            array if not isinstance(inp, MatrixLieGroup) else type(inp)(array)
+            for array, inp in zip(broadcasted_arrays, inputs)
+        ),
+    )

tests/utils.py
@@ -3,14 +3,13 @@ import random
 from typing import Any, Callable, List, Tuple, Type, TypeVar, cast
 
 import jax
+import jaxlie
 import numpy as onp
 import pytest
 import scipy.optimize
 from hypothesis import given, settings
 from hypothesis import strategies as st
 from jax import numpy as jnp
-
-import jaxlie
 
 # Run all tests with double-precision.
 jax.config.update("jax_enable_x64", True)
@@ -45,7 +44,7 @@ def sample_transform(Group: Type[T], batch_axes: Tuple[int, ...] = ()) -> T:
 
 def general_group_test(
     f: Callable[[Type[jaxlie.MatrixLieGroup], Tuple[int, ...]], None],
-    max_examples: int = 10,
+    max_examples: int = 30,
 ) -> Callable[[Type[jaxlie.MatrixLieGroup], Tuple[int, ...], Any], None]:
     """Decorator for defining tests that run on all group types."""
 
@@ -78,6 +77,7 @@ def general_group_test(
         [
             (),
             (1,),
+            (3, 1, 2, 1),
         ],
     )(f_wrapped)
     return f_wrapped
@@ -96,11 +96,11 @@ def assert_transforms_close(a: jaxlie.MatrixLieGroup, b: jaxlie.MatrixLieGroup):
     p1 = jnp.asarray(a.parameters())
     p2 = jnp.asarray(b.parameters())
     if isinstance(a, jaxlie.SO3):
-        p1 = p1 * jnp.sign(jnp.sum(p1, axis=-1))
-        p2 = p2 * jnp.sign(jnp.sum(p2, axis=-1))
+        p1 = p1 * jnp.sign(jnp.sum(p1, axis=-1, keepdims=True))
+        p2 = p2 * jnp.sign(jnp.sum(p2, axis=-1, keepdims=True))
     elif isinstance(a, jaxlie.SE3):
-        p1 = p1.at[..., :4].mul(jnp.sign(jnp.sum(p1[..., :4], axis=-1)))
-        p2 = p2.at[..., :4].mul(jnp.sign(jnp.sum(p2[..., :4], axis=-1)))
+        p1 = p1.at[..., :4].mul(jnp.sign(jnp.sum(p1[..., :4], axis=-1, keepdims=True)))
+        p2 = p2.at[..., :4].mul(jnp.sign(jnp.sum(p2[..., :4], axis=-1, keepdims=True)))
 
     # Make sure parameters are equal.
     assert_arrays_close(p1, p2)