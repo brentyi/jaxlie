b41953bddd7faefa8129e16859a9af174a96cf04
Brent Yi <yibrenth@gmail.com>
2024-05-05 23:02:24 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="jaxlie",
-    version="1.3.4",
+    version="1.4.0",
     description="Matrix Lie groups in JAX",
     long_description=long_description,
     long_description_content_type="text/markdown",
